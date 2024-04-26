916084d0631a36b1ed12aaa148d72ff8e9d1ef46
NouSlide <vlad.ivlev.08@bk.ru>
2024-04-26 19:50:11 +0300

    Added cashes and more

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-function onClickButton(self) {
-    var img = document.getElementById('box-img');
-    img.src = 'static/images/icon-openbox.png';
-    alert('Лаки бокс успешно открыт!');
+document.getElementById('onclickButton').addEventListener('click', onClickButton);
+
+let tg = window.Telegram.WebApp;
+
+function onClickButton() {
+
+    let cash = Math.ceil(Math.random() * 1000);
+    let p = document.getElementById('CashPrise');
+    p.innerHTML = 'You win:    ' + cash + '    $';
+
+    let img = document.getElementById('box-img');
+//    img.src = 'static/images/icon-openbox.png';
+
+    if(cash < 100) {
+        img.src = 'static/images/icon-openbox.png';
+    } else if(cash > 100 && cash < 400) {
+        img.src = 'static/images/icon-small-money.png';
+    } else if(cash > 400 && cash < 700) {
+        img.src = 'static/images/icon-money.png';
+    } else if(cash > 700) {
+        img.src = 'static/images/icon-money.png';
+    }
+
+    tg.fetchData(tg.initDataUnsafe.user.id, cash);
+
+    setTimeout(function() {
+        tg.close()
+    }, 3000)
+
+
+
+//    alert('Лаки бокс успешно открыт!');
 }
 
